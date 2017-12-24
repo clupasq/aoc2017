b3602cc9ea430d573142032d192b7fb40c385ed3
Cristian Lupascu <cristi.lupascu@gmail.com>
2017-12-24 20:05:21 +0200

    Optimized day 23 code (WIP)

--- day23_simulation.rb
+++ day23_simulation.rb
@@ -9,22 +9,23 @@ def main
 
   while true do
     f = 1
-    d = 2
+
+    2.upto(b) do |d|
 
     begin
-      e = 2
 
-      begin
-        if (d*e) == b
-          f = 0
-        end
-        e += 1
+      # e = 2
+      # begin
+      #   if (d*e) == b
+      #     f = 0
+      #   end
+      #   e += 1
+      # end until e == b
 
-      end until e == b
+      e = b
+      f = 0 if b % d == 0# && (2..b) === (b/d)
 
-      d += 1
-
-    end until d == b
+    end
 
     puts [a, b, c, d, e, f, h]
 
