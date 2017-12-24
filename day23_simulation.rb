812fe7e51946c3236f5789703fffedd4def74b40
Cristian Lupascu <cristi.lupascu@gmail.com>
2017-12-24 20:25:58 +0200

    Solve day23 - part 2

--- day23_simulation.rb
+++ day23_simulation.rb
@@ -1,47 +1,57 @@
 
 
+require 'prime'
 
-def main
-  a = 1
 
+def main_optimized
   b = 84 * 100 + 100000
   c = b + 17000
+  test = (b..c).step(17).to_a
 
-  while true do
-    f = 1
-
-    2.upto(b) do |d|
-
-    begin
-
-      # e = 2
-      # begin
-      #   if (d*e) == b
-      #     f = 0
-      #   end
-      #   e += 1
-      # end until e == b
-
-      e = b
-      f = 0 if b % d == 0# && (2..b) === (b/d)
-
-    end
-
-    puts [a, b, c, d, e, f, h]
-
-    if f != 0
-      h += 1
-    end
-
-    if b == c
-      puts h
-      return
-    end
-
-    b += 17
-  end
-
+  test.count { |n| not n.prime? }
 end
 
-main
+puts main_optimized
+
+# def main
+#   a = 1
+#
+#   b = 84 * 100 + 100000
+#   c = b + 17000
+#
+#   while true do
+#     f = 1
+#
+#     2.upto(b) do |d|
+#
+#     begin
+#
+#       # e = 2
+#       # begin
+#       #   if (d*e) == b
+#       #     f = 0
+#       #   end
+#       #   e += 1
+#       # end until e == b
+#
+#       e = b
+#       f = 0 if b % d == 0# && (2..b) === (b/d)
+#
+#     end
+#
+#     puts [a, b, c, d, e, f, h]
+#
+#     if f = 0
+#       h += 1
+#     end
+#
+#     if b == c
+#       puts h
+#       return
+#     end
+#
+#     b += 17
+#   end
+#
+# end
 
