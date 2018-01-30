4c55b788af815a6b6612999917c096c8761f8045
Cristian Lupascu <cristi.lupascu@gmail.com>
2018-01-30 10:27:58 +0200

    Faster version for day5 using Sequences

--- day5.hs
+++ day5.hs
@@ -1,5 +1,8 @@
 import Test.Hspec
 
+import qualified Data.Sequence as S
+
+-- initial (slow) implementation with lists
 type ListWithIndex = ([Int], Int)
 
 step1 :: ListWithIndex -> ListWithIndex
@@ -21,38 +24,60 @@ step2 (xs, i) = (xs', i')
                        else current + 1
         xs'       = prefix ++ [newOffset] ++ suffix
 
-
 countSteps :: (ListWithIndex -> ListWithIndex) -> ListWithIndex -> Int
-countSteps step1fn = length . takeWhile insideBounds . iterate step1fn
+countSteps stepFn = length . takeWhile insideBounds . iterate stepFn
+  where insideBounds (xs', i') = i' >= 0 && i' < length xs'
+
+
+-- faster, using Data.Sequence
+
+type SeqWithIndex = (S.Seq Int, Int)
+
+step1' :: SeqWithIndex -> SeqWithIndex
+step1' (xs, i) = (xs', i')
+  where i'  = i + xs `S.index` i
+        xs' = S.adjust (+1) i xs
+
+step2' :: SeqWithIndex -> SeqWithIndex
+step2' (xs, i) = (xs', i')
+  where i'    = i + xs `S.index` i
+        upd n | n > 2     = pred n
+              | otherwise = succ n
+        xs'   = S.adjust upd i xs
+
+countSteps' :: (SeqWithIndex -> SeqWithIndex) -> SeqWithIndex -> Int
+countSteps' stepFn = length . takeWhile insideBounds . iterate stepFn
   where insideBounds (xs', i') = i' >= 0 && i' < length xs'
 
 main = hspec $
 
   describe "List jumping" $ do
 
+
     it "can make an iteration" $ do
 
-       step1 ([0, 3, 0, 1, -3], 0) `shouldBe` ([1, 3, 0, 1, -3], 0)
-       step1 ([1, 3, 0, 1, -3], 0) `shouldBe` ([2, 3, 0, 1, -3], 1)
-       step1 ([2, 3, 0, 1, -3], 1) `shouldBe` ([2, 4, 0, 1, -3], 4)
-       step1 ([2, 4, 0, 1, -3], 4) `shouldBe` ([2, 4, 0, 1, -2], 1)
-       step1 ([2, 4, 0, 1, -2], 1) `shouldBe` ([2, 5, 0, 1, -2], 5)
+       step1' (S.fromList [0, 3, 0, 1, -3], 0) `shouldBe` (S.fromList [1, 3, 0, 1, -3], 0)
+       step1' (S.fromList [1, 3, 0, 1, -3], 0) `shouldBe` (S.fromList [2, 3, 0, 1, -3], 1)
+       step1' (S.fromList [2, 3, 0, 1, -3], 1) `shouldBe` (S.fromList [2, 4, 0, 1, -3], 4)
+       step1' (S.fromList [2, 4, 0, 1, -3], 4) `shouldBe` (S.fromList [2, 4, 0, 1, -2], 1)
+       step1' (S.fromList [2, 4, 0, 1, -2], 1) `shouldBe` (S.fromList [2, 5, 0, 1, -2], 5)
 
     it "can count number of steps" $ do
 
-       countSteps step1 ([0, 3, 0, 1, -3], 0) `shouldBe` 5
-       countSteps step2 ([0, 3, 0, 1, -3], 0) `shouldBe` 10
+       countSteps' step1' (S.fromList [0, 3, 0, 1, -3], 0) `shouldBe` 5
+       countSteps' step2' (S.fromList [0, 3, 0, 1, -3], 0) `shouldBe` 10
 
 
     it "solves #1" $ do
 
       inputText <- readFile "input/day5.in"
-      let xs = map read $ lines inputText
-      print $ countSteps step1 (xs, 0)
+      let xs = S.fromList $ map read $ lines inputText
+      print $ countSteps' step1' (xs, 0)
 
     it "solves #2" $ do
+      -- still slow... ~257s
 
-      pending
-      -- todo: make this faster if possible
-      -- print $ countSteps step2 (xs, 0)
+      inputText <- readFile "input/day5.in"
+      let xs = S.fromList $ map read $ lines inputText
+      print $ countSteps' step2' (xs, 0)
 
